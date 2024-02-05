354700c86c06b87bfe6674e344eb53bb8337d126
muhammed-jahas <jahaspty1@gmail.com>
2024-02-05 12:26:46 +0530

    fix: update database config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+// mongoose.connect('mongodb://127.0.0.1:27017/trudd',{})
+// .then(()=>{
+//   console.log('MongoDB Connected');
+// })
+// .catch((err)=>{
+//   console.log('MongoDB Connection Error', err);
+// });
 
-mongoose.connect('mongodb://127.0.0.1:27017/trudd',{})
-.then(()=>{
-  console.log('MongoDB Connected');
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
 })
-.catch((err)=>{
-  console.log('MongoDB Connection Error', err);
-});
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log('MongoDB Connection Error', err);
+  });
 
 module.exports = mongoose.connection;
